0ab335d3c42689f5cd103158c6bd4f289104e470
Zayd EL IMRANI <elimrani.z@gmail.com>
2023-01-15 06:56:49 +0100

    Make Swagger public

--- API/Program.cs
+++ API/Program.cs
@@ -31,11 +31,11 @@ builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins",
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors("SuperHeroOrigins");
 
